4875f4a1297a358d1c4358bb4e92bd190453e78b
Nick <nick.park@student.manchester.ac.uk>
2016-08-03 16:33:39 +0100

    simple gui works

--- ImageUploader.java
+++ ImageUploader.java
@@ -5,13 +5,34 @@ import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 
-public class ImageUploader extends JFrame
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class ImageUploader extends JPanel
 {
+  private static BufferedImage image;
+
+  @Override
+  public void paintComponent(Graphics g)
+  {
+    super.paintComponent(g);
+    g.drawImage(image, 0, 0, this);
+  } // paintComponent
+
   public static void main(String[] args) throws IOException
   {
+    JFrame frame = new JFrame();
+    JPanel panel = new ImageUploader();
+    frame.getContentPane().add(panel);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(350, 350);
+    frame.setVisible(true);
+
     File input = new File("8bit_mushroom_intro.jpg");
-    BufferedImage image = ImageIO.read(input);
-    ImageFormatter imageFormatter = new ImageFormatter(image);
+    image = ImageIO.read(input);
+    ImageFormatter imageFormatter = new ImageFormatter(image, panel);
+
+    panel.repaint();
 
     imageFormatter.formatImage();
   } // main
--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.Color;
 import java.io.File;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -9,27 +10,31 @@ import java.io.IOException;
 
 public class Test extends JPanel
 {
-
-   @Override
-   public void paint(Graphics g)
-   {
-      try
-      {
-        Image img = ImageIO.read(new File("8bit_mushroom_intro.jpg"));
-        g.drawImage(img, 0, 0, this);
-      }
-      catch (IOException e){}
-   }
+  private static BufferedImage img;
+  //  @Override
+  //  public void paint(Graphics g)
+  //  {
+  //     try
+  //     {
+  //       Image img = ImageIO.read(new File("8bit_mushroom_intro.jpg"));
+  //       g.drawImage(img, 0, 0, this);
+  //     }
+  //     catch (IOException e){}
+  //  }
 
    @Override
    public void paintComponent(Graphics g)
    {
      super.paintComponent(g);
-     g.fillRect(0, 0, 100, 100);
+     //g.fillRect(0, 0, 100, 100);
+     //g.fillArc(0, 0, 200, 200, 270, 90);
+     g.drawImage(img, 0, 0, this);
    }
 
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {
+      img = ImageIO.read(new File("8bit_mushroom_intro.jpg"));
+
       JFrame frame = new JFrame();
       JPanel panel = new Test();
       frame.getContentPane().add(panel);
@@ -38,9 +43,10 @@ public class Test extends JPanel
       frame.setSize(350, 350);
       frame.setVisible(true);
 
-      // Graphics g = panel.getGraphics();
-      // g.fillRect(0, 0, 100, 100);
-      // panel.paintComponent(g);
-      frame.repaint();
+      for(int x=0; x < 50; x++)
+        for(int y=0; y < 50; y++)
+          img.setRGB(x, y, new Color(0, 0, 0).getRGB());
+
+      panel.repaint();
    }
 }
